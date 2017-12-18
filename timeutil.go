60307ec77397512143f036bb3054be2082fe79e3
azihsoyn <azihsoyn@gmail.com>
2017-12-18 19:41:53 +0900

    initial release

---
+++ timeutil.go
@@ -0,0 +1,79 @@
+package timeutil
+
+import "time"
+
+type Time struct {
+	time.Time
+}
+
+type Operator string
+
+const (
+	GT Operator = "gt"
+	GE          = "ge"
+	LT          = "lt"
+	LE          = "le"
+	EQ          = "eq"
+)
+
+func (t Time) GreaterThan(other time.Time) bool {
+	return t.GT(other)
+}
+
+func (t Time) GT(other time.Time) bool {
+	return t.After(other)
+}
+
+func (t Time) GE(other time.Time) bool {
+	return !t.Before(other)
+}
+
+func (t Time) GreaterEqual(other time.Time) bool {
+	return t.GE(other)
+}
+
+func (t Time) LT(other time.Time) bool {
+	return t.Before(other)
+}
+
+func (t Time) LessThan(other time.Time) bool {
+	return t.LT(other)
+}
+
+func (t Time) LE(other time.Time) bool {
+	return !t.After(other)
+}
+
+func (t Time) LessEqual(other time.Time) bool {
+	return t.LE(other)
+}
+
+func (t Time) Between(t1, t2 time.Time) bool {
+	return (Time{t1}).LessEqual(t.Time) && t.LessEqual(t2)
+}
+
+func (t Time) EQ(other time.Time) bool {
+	return t.Time.Equal(other)
+}
+
+func (t Time) Equal(other time.Time) bool {
+	return t.EQ(other)
+}
+
+func Eval(t1 time.Time, op Operator, t2 time.Time) bool {
+	t := Time{t1}
+	switch op {
+	case GT:
+		return t.GT(t2)
+	case GE:
+		return t.GE(t2)
+	case LT:
+		return t.LT(t2)
+	case LE:
+		return t.LE(t2)
+	case EQ:
+		return t.EQ(t2)
+	default:
+		return false
+	}
+}
---
+++ timeutil_test.go
@@ -0,0 +1,201 @@
+package timeutil
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var loc = time.Local
+
+func Test_GreaterThan(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		t      time.Time
+		other  time.Time
+		expect bool
+	}{
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			expect: true,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			expect: false,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			expect: false,
+		},
+	}
+
+	for i, test := range tests {
+		assert.Equal(test.expect, (Time{test.t}).GreaterThan(test.other), i)
+		assert.Equal(test.expect, (Time{test.t}).GT(test.other), i)
+	}
+}
+
+func Test_GreaterEqual(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		t      time.Time
+		other  time.Time
+		expect bool
+	}{
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			expect: true,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			expect: true,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			expect: false,
+		},
+	}
+
+	for i, test := range tests {
+		assert.Equal(test.expect, (Time{test.t}).GreaterEqual(test.other), i)
+		assert.Equal(test.expect, (Time{test.t}).GE(test.other), i)
+	}
+}
+
+func Test_LessThan(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		t      time.Time
+		other  time.Time
+		expect bool
+	}{
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			expect: false,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			expect: false,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			expect: true,
+		},
+	}
+
+	for i, test := range tests {
+		assert.Equal(test.expect, (Time{test.t}).LessThan(test.other), i)
+		assert.Equal(test.expect, (Time{test.t}).LT(test.other), i)
+	}
+}
+
+func Test_LessEqual(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		t      time.Time
+		other  time.Time
+		expect bool
+	}{
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			expect: false,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			expect: true,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			expect: true,
+		},
+	}
+
+	for i, test := range tests {
+		assert.Equal(test.expect, (Time{test.t}).LessEqual(test.other), fmt.Sprintf("No. %d", i+1))
+		assert.Equal(test.expect, (Time{test.t}).LE(test.other), fmt.Sprintf("No. %d", i+1))
+	}
+}
+
+func Test_Equal(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		t      time.Time
+		other  time.Time
+		expect bool
+	}{
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			expect: false,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			other:  time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			expect: true,
+		},
+	}
+
+	for i, test := range tests {
+		assert.Equal(test.expect, (Time{test.t}).Equal(test.other), fmt.Sprintf("No. %d", i+1))
+		assert.Equal(test.expect, (Time{test.t}).EQ(test.other), fmt.Sprintf("No. %d", i+1))
+	}
+}
+
+func Test_Between(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		t      time.Time
+		t1     time.Time
+		t2     time.Time
+		expect bool
+	}{
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 0, 0, loc),
+			t1:     time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			t2:     time.Date(2017, 12, 18, 0, 0, 3, 0, loc),
+			expect: false,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			t1:     time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			t2:     time.Date(2017, 12, 18, 0, 0, 3, 0, loc),
+			expect: true,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 2, 0, loc),
+			t1:     time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			t2:     time.Date(2017, 12, 18, 0, 0, 3, 0, loc),
+			expect: true,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 2, 0, loc),
+			t1:     time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			t2:     time.Date(2017, 12, 18, 0, 0, 3, 0, loc),
+			expect: true,
+		},
+		{
+			t:      time.Date(2017, 12, 18, 0, 0, 4, 0, loc),
+			t1:     time.Date(2017, 12, 18, 0, 0, 1, 0, loc),
+			t2:     time.Date(2017, 12, 18, 0, 0, 3, 0, loc),
+			expect: false,
+		},
+	}
+
+	for i, test := range tests {
+		assert.Equal(test.expect, (Time{test.t}).Between(test.t1, test.t2), fmt.Sprintf("No. %d", i+1))
+	}
+}
